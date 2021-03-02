baded16a799708edfa2b789475b131cf4e71bc8d
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-02 16:59:11 +0100

    WIP vue

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,21 @@
   \********************/
 /***/ (() => {
 
+var app = new Vue({
+  el: "#app",
+  data: {
+    dischi: []
+  },
+  mounted: function mounted() {
+    var _this = this;
 
+    axios.get('server.php').then(function (response) {
+      _this.dischi = response.data;
+      console.log(_this.dischi);
+    });
+  },
+  methods: {}
+});
 
 /***/ }),
 
